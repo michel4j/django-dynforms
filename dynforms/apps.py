fdb7d8db43807b5ba98f26300b3c6f6fd92bd57f
Michel Fodje <michel4j@gmail.com>
2025-06-28 11:50:28 -0600

    Set default auto field type to BigAutoField in DynformsConfig

--- dynforms/apps.py
+++ dynforms/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DynformsConfig(AppConfig):
     name = 'dynforms'
     verbose_name = "Dynamic Forms"
+    default_auto_field = 'django.db.models.BigAutoField'
